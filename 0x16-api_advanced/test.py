37f1fbc235b4181ff6fd8af31b279f88d615e421
IsaiahKipyego <kipyegoisaiah@gmail.com>
2024-01-16 11:36:19 -0800

    API advanced

--- 0x16-api_advanced/test.py
+++ 0x16-api_advanced/test.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
-"""
-script that finds top 10 hot posts in a subreddit
-"""
 import requests
 
 
-def top_ten(subreddit):
-    """
-    gets top ten hot posts from subreddit passed as an argument
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'api-request by Namasaka'}
-    res = requests.get(url, headers=headers, allow_redirects=False)
+def number_of_subscribers(subreddit):
+    '''module that gets total number of subscribers from reddit api'''
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:73.0) \
+        Gecko/20100101 Firefox/73.0"
+    }
 
-    if res.status_code == 200:
-        res = res.json()
-        posts = res['data']['children']
-        # print the first 10 items
-        print(len(posts))
-        print(res['data']['dist'])
-        i = 1
-        for post in posts:
-            print('{}. {}'.format(i, post['data']['title']))
-            i += 1
-    else:
-        print('None')
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    return response.json().get('data').get('subscribers')
