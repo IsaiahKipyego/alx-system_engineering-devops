4b77904ddaf9b05d4f3edc0b0c823859e1759498
IsaiahKipyego <kipyegoisaiah@gmail.com>
2024-01-25 18:59:16 +0000

    Web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# fixes the limit file at hbton user.
+
+exec { 'fix_limit_hbton_user':
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increases soft file limit hbton ser.
+exec { 'increase_soft_file':
+  command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
